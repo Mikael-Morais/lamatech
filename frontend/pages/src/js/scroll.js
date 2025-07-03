3b3c675fcf9d6a6288dc006289a6a2c22fe74472
Myguel-Angelo <angelo.nunes576@gmail.com>
2025-07-03 20:29:53 -0300

    uptade: folder organization

---
+++ frontend/pages/src/js/scroll.js
@@ -0,0 +1,14 @@
+const animatedElements = document.querySelectorAll('.animate-on-scroll');
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+            observer.unobserve(entry.target);
+        }
+    });
+}, {
+    threshold: 0.1
+});
+animatedElements.forEach(element => {
+    observer.observe(element);
+});
